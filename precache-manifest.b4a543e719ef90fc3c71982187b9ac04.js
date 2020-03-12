dba12efafa0cc3042382d8e3570083da0826e6ac
pHarish290401 <paladuguharish100@gmail.com>
2020-03-12 15:40:22 +0530

    Updates

---
+++ precache-manifest.b4a543e719ef90fc3c71982187b9ac04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcedb1aeece9cdd5205ac18f65a9bd51",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "e10320b96c69c7713ac3",
+    "url": "/project5/static/css/main.c173e436.chunk.css"
+  },
+  {
+    "revision": "5274862506ac7fb8fbdc",
+    "url": "/project5/static/js/2.d9dfb6be.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project5/static/js/2.d9dfb6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10320b96c69c7713ac3",
+    "url": "/project5/static/js/main.6c311274.chunk.js"
+  },
+  {
+    "revision": "6189902f101415fbb401",
+    "url": "/project5/static/js/runtime-main.03089d41.js"
+  },
+  {
+    "revision": "796737bd2aa9a67ce24f278cafbe3772",
+    "url": "/project5/static/media/people.796737bd.svg"
+  }
+]);
